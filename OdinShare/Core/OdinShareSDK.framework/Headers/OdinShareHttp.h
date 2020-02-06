c210c3a9b8ab9af6b27b98c0c9d599485bf488cd
苏波 <syyjay@sina.cn>
2020-02-06 15:24:04 +0800

    -- SDK升级 域名安全

--- OdinShare/Core/OdinShareSDK.framework/Headers/OdinShareHttp.h
+++ OdinShare/Core/OdinShareSDK.framework/Headers/OdinShareHttp.h
@@ -12,6 +12,10 @@ NS_ASSUME_NONNULL_BEGIN
 
 @interface OdinShareHttp : NSObject
 
++ (instancetype)shareInstance;
+
++ (NSString *)getBaseUrl:(BOOL)onceAgin;
+
 + (void)post:(NSString *)urlString parameters:(nullable id)parameters success:(nullable void (^)(id _Nullable responseObject))success failure:(nullable void (^)(NSError *error))failure;
 
 + (void)get:(NSString *)urlString parameters:(nullable id)parameters header:(nullable id)header success:(nullable void (^)(id _Nullable responseObject))success failure:(nullable void (^)(NSError *error))failure;
